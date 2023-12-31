98f4099a1802f96145b49fa6198efde0eca1eb02
Subhashish Samantaray <subhashishsamantaray@Subhashishs-MacBook-Pro-2nd.local>
2023-12-31 17:31:47 +0530

    updates

--- src/screen/Office.js
+++ src/screen/Office.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { BreadCumb } from '../component/BreadCumb'
+import { Content } from './Office/Content'
 
 export const Office = () => {
   return (
-    <div>Office</div>
+<>
+<BreadCumb bgImage={"url('assets/img/banner/office.jpeg')"} routeLink={"office"} mainRoute={"Home"} 
+    route={"Office"} title={"Our Office"}/>
+<Content/>
+</>
   )
 }
---
+++ src/screen/Office/Content.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Content = () => {
+  return (
+    <div style={{minHeight:200,textAlign:'center',paddingTop:100}}>Under Development</div>
+  )
+}
